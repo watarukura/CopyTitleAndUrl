9024bc718c6bdf8964f1269a06957bba457936e6
wataru-kurashima <wataru-kurashima@hands-lab.com>
2020-11-25 09:52:05 +0900

    feat(notion): notionのtitle＋URLのコピーに対応

--- copy.js
+++ copy.js
@@ -12,29 +12,19 @@ javascript:(
             document.querySelectorAll("link")
                 .forEach((item) => { if (item.href.indexOf("alias") != -1) {wiki_url = item.href} });
             const wiki_title = document.querySelector("title").innerText.match(/\] (.*) \|/)[1];
-            title_and_url = "[" + wiki_title + "](" + wiki_url + ")";
+            title_and_url = `[${wiki_title}](${wiki_url})`;
         } else if (url.indexOf("view") != -1) {
             const title = document.querySelector(".title-group__title-text").innerText;
             const project_key = url.split("/").pop();
-            title_and_url =  "[" + project_key + " " + title + "]" + "(" + url + ")";
+            title_and_url =  `[${project_key} ${title}](${url})`;
+        } else if (url.indexOf("notion.so") != -1) {
+            const title = document.querySelector("title").innerText
+            title_and_url =  `[${title}](${url})`;
+            console.log(title_and_url)
         } else {
             alert("failed.");
             return null;
         }
-        // execCopy https://qiita.com/simiraaaa/items/2e7478d72f365aa48356
-        const temp = document.createElement('div');
-        temp.appendChild(document.createElement('pre')).textContent = title_and_url;
-        const s = temp.style;
-        s.position = 'fixed';
-        s.left = '-100%';
-        document.body.appendChild(temp);
-        document.getSelection().selectAllChildren(temp);
-        const result = document.execCommand('copy');
-        document.body.removeChild(temp);
-        if (result) {
-            alert("copied!")
-        } else {
-            alert("copy failed...")
-        }
+        navigator.clipboard.writeText(title_and_url).then(() => console.log("copied!"))
     }
 )();
