b3ec48d0479c793497e0d9484802a8863db8b92e
Wataru Kurashima <what.r.j150@gmail.com>
2021-12-04 09:39:29 +0900

    style: 💄 refactoring

--- copy.js
+++ copy.js
@@ -1,27 +1,22 @@
-// ==ClosureCompiler==
-// @compilation_level SIMPLE_OPTIMIZATIONS
-// @formatting pretty_print
-// ==/ClosureCompiler==
-
 javascript:(
     function(){
         const url = location.href.replace(/#.*/, '');
         let title_and_url = "";
-        if (url.indexOf("wiki") != -1) {
+        if (url.includes("wiki")) {
             let wiki_url = "";
             document.querySelectorAll("link")
-                .forEach((item) => { if (item.href.indexOf("alias") != -1) {wiki_url = item.href} });
+                .forEach((item) => { if (item.href.includes("alias")) {wiki_url = item.href} });
             const wiki_title = document.querySelector("title").innerText.match(/\] (.*) \|/)[1];
             title_and_url = `[${wiki_title}](${wiki_url})`;
-        } else if (url.indexOf("view") != -1) {
+        } else if (url.includes("view")) {
             const title = document.querySelector(".title-group__title-text").innerText;
             const project_key = url.split("/").pop();
             title_and_url =  `[${project_key} ${title}](${url})`;
-        } else if (url.indexOf("notion.so") != -1) {
+        } else if (url.includes("notion.so")) {
             const title = document.querySelector("title").innerText
             title_and_url =  `[${title}](${url})`;
             console.log(title_and_url)
-        } else if (url.indexOf("github.com") != -1) {
+        } else if (url.includes("github.com")) {
             const title = document.querySelector(".gh-header-title").innerText
             title_and_url =  `[${title}](${url})`;
             console.log(title_and_url)
