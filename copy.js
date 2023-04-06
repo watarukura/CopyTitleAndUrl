202c908f2f8394a58ea9ca7d26d97194443a2f63
Wataru Kurashima <what.r.j150@gmail.com>
2023-04-06 15:23:27 +0900

    feat: 🎸 simplify

--- copy.js
+++ copy.js
@@ -1,26 +1,7 @@
 javascript:(
     function () {
         const url = location.href.replace(/#.*/, '');
-        const title = ((url) => {
-            if (url.includes("backlog.com") || url.includes("backlog.jp")) {
-                if (url.includes("wiki")) {
-                    return document.querySelector("title").innerText.match(/\] (.*) \|/)[1];
-                } else if (url.includes("view")) {
-                    const title = document.querySelector(".title-group__title-text").innerText;
-                    const project_key = url.split("/").pop();
-                    return project_key.concat(' ', title);
-                }
-            } else if (url.includes("notion.so")) {
-                return document.querySelector("title").innerText;
-            } else if (url.includes("github.com")) {
-                return document.querySelector(".gh-header-title").innerText;
-            } else if (url.includes("esa.io")) {
-                return document.querySelector(".floating-header__left").innerText.replace("\n", ": ").replaceAll(" \n", "/");
-            } else if (url.includes("docbase.io")) {
-                return document.querySelector('h2[class^="memo_header"]').innerText;
-            }
-        })(url);
-
+        const title = document.querySelector('title').innerText;
         if (title == null) {
             alert("failed.");
             return;
